f58cc8a43fb43a08006fb7e5139a6635c3f5df87
jesticecrew2 <hpcoc001@gmail.com>
2019-03-12 14:12:05 +0530

    inhertance

---
+++ inheritance.java
@@ -0,0 +1,21 @@
+class A{
+public void car()
+{
+    System.out.println("Audi car");
+}
+}
+class B extends A{
+    public void bike()
+    {
+        System.out.println("Bike");
+    }
+
+}
+
+public class inheritance
+{
+    public static void main(String[] args){
+        B ob=new B();
+        ob.car();
+    }
+}
